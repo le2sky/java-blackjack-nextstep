b88e9cc19c3a3e75fd9a2a4cda4dd2b9bba0f382
le2sky <leehaneul990623@gmail.com>
2023-08-23 22:35:18 +0900

    style : 가독성을 위해서 개행 추가

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final String READ_PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String READ_PLAYER_NAME_MESSAGE =
+            "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String PLAYER_NAME_DELIMITER = ",";
     private static final String READ_MONEY_TEMPLATE = "%s 의 배팅 금액은?";
 
