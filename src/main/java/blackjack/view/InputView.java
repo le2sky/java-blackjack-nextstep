dec0ddd60f61c7255e0c695e5c8edd2220a85a7a
le2sky <leehaneul990623@gmail.com>
2023-08-23 22:31:57 +0900

    refactor : 하드코딩된 값 상수화

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class InputView {
 
+    private static final String READ_PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String PLAYER_NAME_DELIMITER = ",";
+
     public static String[] readPlayerNames() {
-        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        System.out.println(READ_PLAYER_NAME_MESSAGE);
         Scanner scanner = new Scanner(System.in);
 
-        return scanner.nextLine().trim().split(",");
+        return scanner.nextLine()
+                .trim()
+                .split(PLAYER_NAME_DELIMITER);
     }
 
     public static double readMoney(final String name) {
-        System.out.println(name + "의 배팅 금액은?");
+        System.out.format("%s 의 배팅 금액은?", name);
         Scanner scanner = new Scanner(System.in);
 
         return scanner.nextDouble();
