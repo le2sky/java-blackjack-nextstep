2a3b7cde399f6ea60afb3371157921d9b7dbbdc5
le2sky <leehaneul990623@gmail.com>
2023-08-23 22:33:08 +0900

    refactor : 베팅 금액 입력 메시지 상수 추출

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -6,6 +6,7 @@ public class InputView {
 
     private static final String READ_PLAYER_NAME_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String PLAYER_NAME_DELIMITER = ",";
+    private static final String READ_MONEY_TEMPLATE = "%s 의 배팅 금액은?";
 
     public static String[] readPlayerNames() {
         System.out.println(READ_PLAYER_NAME_MESSAGE);
@@ -17,7 +18,7 @@ public class InputView {
     }
 
     public static double readMoney(final String name) {
-        System.out.format("%s 의 배팅 금액은?", name);
+        System.out.format(READ_MONEY_TEMPLATE, name);
         Scanner scanner = new Scanner(System.in);
 
         return scanner.nextDouble();
